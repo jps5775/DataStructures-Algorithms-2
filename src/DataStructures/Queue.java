dae6125fa89239edd13315a2e488699bbd9e3d65
Joe Sema <joesema@Joes-Laptop.local>
2025-02-08 17:34:10 -0800

    added Queue

--- src/DataStructures/Queue.java
+++ src/DataStructures/Queue.java
@@ -1,4 +1,37 @@
 package DataStructures;
 
 public class Queue {
+    Node front;
+    Node back;
+
+    public void enqueue(int val){
+        if(back == null){
+            back = new Node(val);
+            front = back;
+        }else{
+            Node newNode = new Node(val);
+            back.next = newNode;
+            back = back.next;
+        }
+    }
+
+    public int dequeue(){
+        if(front == null){ return -1; }
+
+        int val = front.val;
+        front = front.next;
+        if(front == null){
+            back = null;
+        }
+
+        return val;
+    }
+
+    private class Node{
+        int val;
+        Node next;
+        Node(int val){
+            this.val = val;
+        }
+    }
 }

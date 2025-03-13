d9ff4c2a2f37855208ec7c74f3ca0058598ccdab
Joe Sema <joesema@Joes-Laptop.local>
2025-03-12 23:19:50 -0400

    more binary search

--- src/Algorithms/BinarySearch.java
+++ src/Algorithms/BinarySearch.java
@@ -7,6 +7,48 @@ public class BinarySearch {
         arr = new int[n];
     }
 
+    // e.g. nums = [ 4, 5, 5, 6 ] target = 5
+    //             [ F  T  T  T ]
+
+    // e.g. nums = [ 1, 2, 3, 4 ] target = 5 => value doesn't exist
+    //             [ F  F  F  F ] => returns
+
+    // e.g. nums = [ 11, 12, 15, 20 ] target = 5
+    //             [ T   T   T   T ] => returns 0
+    public int binarySearchFirstValueGreaterOrEqualToTarget(int target){
+        int left = 0;
+        int right = arr.length - 1;
+        int ans = -1; // if there is no value >= target
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(arr[mid] >= target){
+                ans = mid;
+                right = mid - 1; // search for smaller value, so I get the first value >= target
+            }else{
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public int binarySearchFirstValueLessThanOrEqualToTarget(int target){
+        int left = 0;
+        int right = arr.length - 1;
+        int ans = -1;
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(arr[mid] <= target){
+                ans = mid;
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
     public int binarySearch(int target){
         if(arr == null){ return -1; }
 

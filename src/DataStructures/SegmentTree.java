bc884dee8e2de938a5cb8a0e6637177ff941e14f
Joe Sema <joesema@Joes-Laptop.local>
2025-04-12 15:41:45 -0400

    adding segment tree

---
+++ src/DataStructures/SegmentTree.java
@@ -0,0 +1,67 @@
+package DataStructures;
+
+public class SegmentTree {
+    private int[] tree;
+    private int n;
+
+    public SegmentTree(int[] arr) {
+        n = arr.length;
+        tree = new int[4 * n]; // 4n is safe for storing the segment tree
+        build(arr, 0, 0, n - 1);
+    }
+
+    // Build the segment tree
+    private void build(int[] arr, int node, int start, int end) {
+        if (start == end) {
+            // Leaf node
+            tree[node] = arr[start];
+        } else {
+            int mid = (start + end) / 2;
+            build(arr, 2 * node + 1, start, mid);
+            build(arr, 2 * node + 2, mid + 1, end);
+            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        }
+    }
+
+    // Query the sum in range [l, r]
+    public int query(int l, int r) {
+        return query(0, 0, n - 1, l, r);
+    }
+
+    private int query(int node, int start, int end, int l, int r) {
+        if (r < start || l > end) {
+            // No overlap
+            return 0;
+        }
+        if (l <= start && end <= r) {
+            // Total overlap
+            return tree[node];
+        }
+        // Partial overlap
+        int mid = (start + end) / 2;
+        int leftSum = query(2 * node + 1, start, mid, l, r);
+        int rightSum = query(2 * node + 2, mid + 1, end, l, r);
+        return leftSum + rightSum;
+    }
+
+    // Update a value at index i to val
+    public void update(int index, int val) {
+        update(0, 0, n - 1, index, val);
+    }
+
+    private void update(int node, int start, int end, int idx, int val) {
+        if (start == end) {
+            // Leaf node
+            tree[node] = val;
+        } else {
+            int mid = (start + end) / 2;
+            if (idx <= mid) {
+                update(2 * node + 1, start, mid, idx, val);
+            } else {
+                update(2 * node + 2, mid + 1, end, idx, val);
+            }
+            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        }
+    }
+}
+
